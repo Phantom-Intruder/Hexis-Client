1b46fff3b2a1a5636eb6898bccf0216bc48bf1f5
Mewantha <mewa.bandara@gmail.com>
2017-02-13 11:35:50 +0530

    Fixed proloned communication

--- app/src/main/java/hexis/pegasus/com/hexisclient/CallReceiver.java
+++ app/src/main/java/hexis/pegasus/com/hexisclient/CallReceiver.java
@@ -2,9 +2,9 @@ package hexis.pegasus.com.hexisclient;
 
 import android.content.Context;
 import android.os.Vibrator;
+import android.telecom.Call;
 import android.util.Log;
 
-import java.security.acl.LastOwnerException;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -13,32 +13,36 @@ import static android.content.ContentValues.TAG;
 
 public class CallReceiver extends PhonecallReceiver {
 
-    private boolean callEnded;
-    Timer timer;
-    Thread outgoingCallThread;
+    private static Timer timer;
+    private boolean callEnded = true;
 
-    void setCallEnded(boolean value) {
+
+    // now the initialization is done only within the constructor
+
+
+    void setCallEnded(boolean value){
         callEnded = value;
     }
 
-    boolean getCallEnded() {
+    boolean getCallEnded(){
         return callEnded;
     }
 
     @Override
-    protected void onIncomingCallReceived(Context ctx, String number, Date start) {
-        //
+    protected void onIncomingCallReceived(Context ctx, String number, Date start)
+    {
 
     }
 
     @Override
-    protected void onIncomingCallAnswered(Context ctx, String number, Date start) {
-        timer = new Timer();
+    protected void onIncomingCallAnswered(Context ctx, String number, Date start)
+    {
+        CallReceiver.timer = new Timer ();
         final int[] times = {0};
         setCallEnded(false);
-        TimerTask hourlyTask = new TimerTask() {
+        TimerTask hourlyTask = new TimerTask () {
             @Override
-            public void run() {
+            public void run () {
                 if (times[0] == 0) {
                     times[0]++;
                 } else {
@@ -58,65 +62,65 @@ public class CallReceiver extends PhonecallReceiver {
             }
         };
 
-// schedule the task to run starting now and then every hour...
-        timer.schedule(hourlyTask, 0l, 5000);   // 1000*10*60 every 10 minut
+        CallReceiver.timer.schedule (hourlyTask, 0l, 5000);
     }
 
     @Override
-    protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end) {
-        //
+    protected void onIncomingCallEnded(Context ctx, String number, Date start, Date end)
+    {
         setCallEnded(true);
-        Log.d(TAG, "Call stateC" + getCallEnded());
-        try {
-            timer.cancel();
-            timer.purge();
-        } catch (NullPointerException e) {
-            Log.d(TAG, "Error here");
-        }
+        Log.d(TAG, "Call stateC"+getCallEnded());
+        CallReceiver.timer.cancel();
+        CallReceiver.timer.purge();
         HabitThreads prolongedConversationOff = new HabitThreads();
         prolongedConversationOff.talkedForTooLongOff();
     }
 
     @Override
-    protected void onOutgoingCallStarted(Context ctx, String number, Date start) {
+    protected void onOutgoingCallStarted(Context ctx, String number, Date start)
+    {
+        CallReceiver.timer = new Timer ();
         final int[] times = {0};
         setCallEnded(false);
-        outgoingCallThread = new Thread(new Runnable() {
+        TimerTask hourlyTask = new TimerTask () {
             @Override
-            public void run() {
+            public void run () {
+                if ((times[0] == 0)||(times[0] == 1)) {
+                    times[0]++;
+                } else {
+                    if (!getCallEnded()) {
 
-                while (!getCallEnded()) {
-                    try {
-                        Thread.sleep(5000);
-                    } catch (InterruptedException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                    boolean conversationState = MainActivity.getProlongedConversationsState;
-                    if (conversationState) {
-                        Log.d(TAG, "Call stateC" + getCallEnded() + " --- ");
-                        HabitThreads prolongedConversation = new HabitThreads();
-                        prolongedConversation.talkedForTooLong();
+                        boolean conversationState = MainActivity.getProlongedConversationsState;
 
+                        if (conversationState) {
+                            Log.d(TAG, "Call stateC" + getCallEnded() + " --- ");
+                            HabitThreads prolongedConversation = new HabitThreads();
+                            prolongedConversation.talkedForTooLong();
+                        }
+                    } else {
+                        Log.d(TAG, "Prob");
                     }
                 }
             }
-        });
-        outgoingCallThread.start();
+        };
+
+        CallReceiver.timer.schedule (hourlyTask, 0l, 5000);
     }
 
-
     @Override
-    protected void onOutgoingCallEnded(Context ctx, String number, Date start, Date end) {
+    protected void onOutgoingCallEnded(Context ctx, String number, Date start, Date end)
+    {
         setCallEnded(true);
-        Log.d(TAG, "Call stateC" + getCallEnded());
-
+        Log.d(TAG, "Call stateC"+getCallEnded());
+        CallReceiver.timer.cancel();
+        CallReceiver.timer.purge();
         HabitThreads prolongedConversationOff = new HabitThreads();
         prolongedConversationOff.talkedForTooLongOff();
     }
 
     @Override
-    protected void onMissedCall(Context ctx, String number, Date start) {
+    protected void onMissedCall(Context ctx, String number, Date start)
+    {
         //
     }
 
